af7529576165e73d96ce364a1ab4ce90a2344fd8
whiteshadow41 <thebest-peda@web.de>
2023-03-16 23:02:01 +0100

    use weather and countries classes to show desired evaluations

--- src/main/java/de/bcxp/challenge/App.java
+++ src/main/java/de/bcxp/challenge/App.java
@@ -1,4 +1,7 @@
 package de.bcxp.challenge;
+import com.opencsv.CSVReader;
+
+import java.io.*;
 
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
@@ -13,11 +16,22 @@ public final class App {
     public static void main(String... args) {
 
         // Your preparation code …
+        Weather weatherData;
+        Countries countries;
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread: %s%n", dayWithSmallestTempSpread);
+        try {
+            weatherData = new Weather(new CSVReader(new FileReader(App.class.getResource("weather.csv").getPath())));
+            countries = new Countries(new CSVReader(new FileReader(App.class.getResource("countries.csv").getPath())));
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
 
-        String countryWithHighestPopulationDensity = "Some country"; // Your population density analysis function call …
-        System.out.printf("Country with highest population density: %s%n", countryWithHighestPopulationDensity);
+        // Your day analysis function call …
+        System.out.printf("Day with smallest temperature spread: %d%n", weatherData.getDayWithSmallestTemperatureSpread());
+
+        // Your population density analysis function call …
+        System.out.printf("Country with highest population density: %s%n", countries.getCountryWithHighestPopulationDensity());
     }
 }
